a9dd030f0efe515723a06c2da8916b4e68bf9626
Wrighteous <AriAuto@yahoo.com>
2023-11-28 09:11:38 -0500

    YAY, I have finally made it work, it took me a few days to figure out how to make the grid of squares responsive, and I did use grid instead of flexbox, to make it a little more challenging. I still want to go back and remake it using the flexbox way so that I can learn that too. I will come back very soon and correct the position of the copyright and add a github link to it, also I will add a etch-TA-sketch logo somewhere on the toy, as well as style the buttons differently to make the game look more attractive. Lastly I will add a classic mode that uses the keyboard controls to move the knobs and control the direction of the sketch, in that mode the

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 /*javascript goes here*/
-let defaultColor=1;
-let defaultSize=3;
+let defaultColor=1,
+    defaultSize=50,
+    columns=61,
+    rows=30,
+    changeId='',
+    num=0;
 const master=document.querySelector("#masterBox");
 master.classList.add('master');
-/*display square*/
+
+/*display screen*/
 const display=document.createElement("div");
 display.classList.add("display");
 master.appendChild(display);
@@ -14,11 +19,52 @@ controls.classList.add("controlBox");
 master.appendChild(controls);
 
 /*squares*/
-const square=document.createElement('div');
-square.classList.add('square');
-display.appendChild(square);
-///////////////////////////////make a shit ton of squares
-
+///////create fuction that makes the squares get progressivly darker until
+/////////////////eventually being completely black (10 shades to black)
+const createSquare=index=>{
+  const square=document.createElement('div');
+  square.classList.add('square');
+  changeId=`square${num}`;
+  square.setAttribute('id',changeId);
+  display.appendChild(square);
+  square.addEventListener('mouseover',()=>{
+    square.classList.remove('black','psych1','psych2','psych3','psych4','psych5');
+    if(defaultColor==1){
+      square.classList.add('black');
+    }else if(defaultColor==2){
+      square.classList.add('psych1');
+      defaultColor=3;
+    }else if(defaultColor==3){
+      square.classList.add('psych2');
+      defaultColor=4;
+    }else if(defaultColor==4){
+      square.classList.add('psych3');
+      defaultColor=5;
+    }else if(defaultColor==5){
+      square.classList.add('psych4');
+      defaultColor=6;
+    }else if(defaultColor==6){
+      square.classList.add('psych5');
+      defaultColor=2;
+    }
+  });
+  num++
+  return square;
+}
+const createSquares=quantity=>{
+  Array.from(Array(quantity)).map((square,index)=>{
+    createSquare(index);
+  })
+}
+const createGrid=()=>{
+  display.innerHTML="";
+  num=0;
+  display.style.setProperty("--columns",columns);
+  display.style.setProperty("--rows",rows);
+  createSquares(columns*rows);
+}
+createGrid();
+ 
 /*knobs*/
 const knob1=document.createElement("div");
 const knob2=document.createElement("div");
@@ -46,6 +92,18 @@ controls.appendChild(small);
 controls.appendChild(medium);
 controls.appendChild(large);
 
+/*screen eraser*/
+const erase=document.createElement("button");
+erase.textContent="ERASE";
+erase.classList.add('erase');
+erase.addEventListener('click',()=>{
+  const sq=document.getElementsByClassName('square');
+  for(let item of sq){
+  item.classList.remove('black','psych1','psych2','psych3','psych4','psych5');
+  }
+});
+controls.appendChild(erase);
+
 /*color Buttons*/
 const black=document.createElement('button');
 const psych=document.createElement('button');
@@ -68,70 +126,30 @@ function toBlack(){
 controls.appendChild(black);
 controls.appendChild(psych);
 
-/*screen eraser*/
-const erase=document.createElement("button");
-erase.textContent="ERASE";
-erase.addEventListener('click',clearAll);
-function clearAll(){
-  square.classList.remove('black');
-  square.classList.remove('psych1');
-  square.classList.remove('psych2');
-  square.classList.remove('psych3');
-  square.classList.remove('psych4');
-  square.classList.remove('psych5');
-}
-
-/*erase button and right knob placement*/
-controls.appendChild(erase);
+/*right knob placement*/
 controls.appendChild(knob2);
-
-
-
-/*mouseOver event changer*/
-square.addEventListener("mouseover",changeColor);
-function changeColor(){
-  clearAll();
-  if(defaultColor==1){
-    square.classList.add('black');
-  }else if(defaultColor==2){
-    square.classList.add('psych1');
-    defaultColor=3;
-  }else if(defaultColor==3){
-    square.classList.add('psych2');
-    defaultColor=4;
-  }else if(defaultColor==4){
-    square.classList.add('psych3');
-    defaultColor=5;
-  }else if(defaultColor==5){
-    square.classList.add('psych4');
-    defaultColor=6;
-  }else if(defaultColor==6){
-    square.classList.add('psych5');
-    defaultColor=2;
-  }
-}
-
-///////create fuction that makes the squares get progressivly darker until
-/////////////////eventually being completely black (10 shades to black)
 
 /*square size changers*/
 function removeSizeClass(){
-  square.classList.remove('small');
-  square.classList.remove('medium');
-  square.classList.remove('large');
   small.classList.remove('darker');
   medium.classList.remove('darker');
   large.classList.remove('darker');
 }function toSmall(){
+  columns=145;
+  rows=70;
   removeSizeClass();
   small.classList.add('darker');
-  square.classList.add('small');
+  createGrid();
 }function toMedium(){
+  columns=103;
+  rows=50
   removeSizeClass();
   medium.classList.add('darker');
-  square.classList.add('medium');
+  createGrid();
 }function toLarge(){
+  columns=61;
+  rows=30;
   removeSizeClass();
   large.classList.add('darker');
-  square.classList.add('large');
+  createGrid();
 }
